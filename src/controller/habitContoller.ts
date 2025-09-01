ec10a7a0cd5472a12d17caa8f10992882f6a2da9
akshayr1298 <akshayrpanicker005@gmail.com>
2025-09-01 17:03:36 +0530

    habit reset and markeddone

--- src/controller/habitContoller.ts
+++ src/controller/habitContoller.ts
@@ -10,14 +10,56 @@ export const addHabit = async (
 ): Promise<Response | void> => {
   try {
     const response = await habitService.createHabit(req.body, req.user.userId);
+    return res.status(statusCodes.CREATED).json({
+      message: "habit added successfully",
+      code: statusCodes.CREATED,
+      success: true,
+      data: response,
+    });
+  } catch (error: any) {
+    logger.error(error.message);
     return res
-      .status(statusCodes.CREATED)
-      .json({
-        message: "habit added successfully",
-        code: statusCodes.CREATED,
-        success: true,
-        data: response,
-      });
+      .status(error.statusCode)
+      .json({ message: error.message, success: false, code: error.statusCode });
+  }
+};
+
+export const markDone = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  try {
+    const { id } = req.params;
+    const response = await habitService.markDone(id);
+    return res.status(statusCodes.OK).json({
+      message: "habit marked done successfully",
+      code: statusCodes.OK,
+      success: true,
+      data: response,
+    });
+  } catch (error: any) {
+    logger.error(error.message);
+    return res
+      .status(error.statusCode)
+      .json({ message: error.message, success: false, code: error.statusCode });
+  }
+};
+
+export const resetHabit = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  try {
+    const { id } = req.params;
+    const response = await habitService.resetHabit(id);
+    return res.status(statusCodes.OK).json({
+      message: "habit reset successfully",
+      code: statusCodes.OK,
+      success: true,
+      data: response,
+    });
   } catch (error: any) {
     logger.error(error.message);
     return res
--- src/service/habitService.ts
+++ src/service/habitService.ts
@@ -1,11 +1,14 @@
 import { Types } from "mongoose";
 import AppError from "../lib/error";
-import { ConflictError, ServerError } from "../lib/error/error";
+import { ConflictError, NotFoundError, ServerError } from "../lib/error/error";
 import HabitModel, { HabitInterface } from "../models/habit";
 import logger from "../lib/logger";
 
 const habitService = {
-  async createHabit(data: HabitInterface, userId: string): Promise<any> {
+  async createHabit(
+    data: HabitInterface,
+    userId: string
+  ): Promise<HabitInterface> {
     try {
       const { title, description, startDate }: HabitInterface = data;
       const existingHabit = await HabitModel.findOne({ userId, title });
@@ -22,6 +25,47 @@ const habitService = {
         `habbit added succcessfully by userId:${userId} habitId: ${newHabit.id}`
       );
       return newHabit;
+    } catch (error: any) {
+      if (error instanceof AppError) {
+        throw error;
+      } else {
+        throw new ServerError();
+      }
+    }
+  },
+
+  async markDone(id: string): Promise<Partial<HabitInterface> | null> {
+    try {
+      const habit = await HabitModel.findById(id);
+      if (!habit) {
+        throw new NotFoundError("Habit not found.");
+      }
+      const updatedHabit = await HabitModel.findByIdAndUpdate(
+        id,
+        { $push: { completedDates: new Date() } },
+        { new: true }
+      );
+      return updatedHabit;
+    } catch (error: any) {
+      if (error instanceof AppError) {
+        throw error;
+      } else {
+        throw new ServerError();
+      }
+    }
+  },
+
+  async resetHabit(id: string) {
+    try {
+      const habit = await HabitModel.findByIdAndUpdate(
+        id,
+        { completedDates: [] },
+        { new: true }
+      );
+      if (!habit) {
+        throw new NotFoundError("Habit not found.");
+      }
+      return habit;
     } catch (error: any) {
       if (error instanceof AppError) {
         throw error;
