ce58a6e42ce74cb90de6e1afea52760c30cfd1d7
akshayr1298 <akshayrpanicker005@gmail.com>
2025-09-08 15:21:05 +0530

    env config

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -7,13 +7,15 @@ interface IConfig {
   origin: string;
   MONGODB_URI: string;
   jwtSecret: string;
+  NODE_ENV: string;
 }
-
+//* env config for dev
 const development: IConfig = {
   port: process.env.PORT || 4000,
   origin: process.env.ORIGIN ?? "",
   MONGODB_URI: process.env.DBURL ?? "mongodb://localhost:27017/",
   jwtSecret: process.env.SECRET ?? "e8e026ea45db7149c886d0a0af75533b",
+  NODE_ENV: process.env.NODE_ENV ?? "development",
 };
 
 export default development;
